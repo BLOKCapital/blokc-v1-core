332040ad39ac55a0e53da89bf6099a64c2f9beed
Jayesh Yadav <jayeshnyadav497@gmail.com>
2025-10-16 18:03:11 +0530

    updated script

--- script/deploy/arbitrumOne/ArbitrumDeployment.s.sol
+++ script/deploy/arbitrumOne/ArbitrumDeployment.s.sol
@@ -3,26 +3,28 @@ pragma solidity ^0.8.20;
 
 import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
+import { BaseScript } from "script/Base.s.sol";
 import { FacetRegistryBroadcaster } from "src/ccip/Hub/FacetRegistryBroadcaster.sol";
 
-contract ArbitrumDeployment is Script {
-    address internal deployer;
+contract ArbitrumDeployment is BaseScript {
+    address internal registryDeployer;
 
     // Arbitrum One CCIP Router address
     address constant ARBITRUM_CCIP_ROUTER = 0x141fa059441E0ca23ce184B6A78bafD2A517DdE8;
-    
-    // Base Chain Selector for CCIP
-    uint64 constant BASE_CHAIN_SELECTOR = 15971525489660198786;
 
-    function run() public {
-        deployer = msg.sender;
-        
+    // Base Chain Selector for CCIP
+    uint64 constant BASE_CHAIN_SELECTOR = 15_971_525_489_660_198_786;
+
+    function run() public broadcaster {
+        setUp();
+        registryDeployer = msg.sender;
+
         console.log("Deploying to Arbitrum One...");
-        console.log("Deployer:", deployer);
+        console.log("registryDeployer:", registryDeployer);
 
         // Deploy FacetRegistryBroadcaster directly (no proxy needed)
         FacetRegistryBroadcaster broadcaster = new FacetRegistryBroadcaster(
-            deployer, // initialOwner
+            registryDeployer, // initialOwner
             ARBITRUM_CCIP_ROUTER, // ccipRouter
             BASE_CHAIN_SELECTOR // destinationChainSelector
         );
@@ -30,12 +32,11 @@ contract ArbitrumDeployment is Script {
 
         console.log("Destination Chain Selector set to Base:", BASE_CHAIN_SELECTOR);
 
-        
-      //////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////
         console.log("FacetRegistryBroadcaster:", address(broadcaster));
         console.log("CCIP Router Used:", ARBITRUM_CCIP_ROUTER);
-       
-        console.log("Owner:", deployer);
+
+        console.log("Owner:", registryDeployer);
 
         console.log(" Deploy Base contracts using BaseDeployment.s.sol");
         console.log("Set receiver address in broadcaster using:");
--- script/deploy/base/BaseDeployment.s.sol
+++ script/deploy/base/BaseDeployment.s.sol
@@ -2,57 +2,49 @@
 pragma solidity ^0.8.20;
 
 import { Script } from "forge-std/Script.sol";
+import { BaseScript } from "script/Base.s.sol";
 import { console } from "forge-std/console.sol";
 import { FacetRegistry } from "src/facetRegistry/FacetRegistry.sol";
 import { PoolRegistry } from "src/liquidityPoolRegistry/PoolRegistry.sol";
 import { FacetRegistryReceiver } from "src/ccip/Spoke/FacetRegistryReceiver.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract BaseDeployment is Script {
-    address internal deployer;
+contract BaseDeployment is BaseScript {
+    address internal registryDeployer;
 
     // Base CCIP Router address
     address constant BASE_CCIP_ROUTER = 0x881e3A65B4d4a04dD529061dd0071cf975F58bCD;
 
-    function run() public {
-        deployer = msg.sender;
-        
+    function run() public broadcaster {
+        setUp();
+        registryDeployer = msg.sender;
+
         console.log("Deploying to Base Chain...");
-        console.log("Deployer:", deployer);
+        console.log("registryDeployer:", registryDeployer);
         FacetRegistry facetRegistryImpl = new FacetRegistry();
         console.log("FacetRegistry Implementation:", address(facetRegistryImpl));
 
-       
         bytes memory facetRegistryInitData = abi.encodeWithSelector(
             FacetRegistry.initialize.selector,
-            deployer // msg.sender is the owner for now
+            registryDeployer // msg.sender is the owner for now
         );
-        
-        ERC1967Proxy facetRegistryProxy = new ERC1967Proxy(
-            address(facetRegistryImpl),
-            facetRegistryInitData
-        );
+
+        ERC1967Proxy facetRegistryProxy = new ERC1967Proxy(address(facetRegistryImpl), facetRegistryInitData);
         console.log("FacetRegistry Proxy:", address(facetRegistryProxy));
         PoolRegistry poolRegistryImpl = new PoolRegistry();
         console.log("PoolRegistry Implementation:", address(poolRegistryImpl));
 
-        
         bytes memory poolRegistryInitData = abi.encodeWithSelector(
             PoolRegistry.initialize.selector,
-            deployer // its the owner for now 
+            registryDeployer // its the owner for now
         );
-        
-        ERC1967Proxy poolRegistryProxy = new ERC1967Proxy(
-            address(poolRegistryImpl),
-            poolRegistryInitData
-          );
+
+        ERC1967Proxy poolRegistryProxy = new ERC1967Proxy(address(poolRegistryImpl), poolRegistryInitData);
         console.log("PoolRegistry Proxy:", address(poolRegistryProxy));
 
-       
         FacetRegistryReceiver facetRegistryReceiver = new FacetRegistryReceiver();
         console.log("FacetRegistryReceiver:", address(facetRegistryReceiver));
 
-        
         facetRegistryReceiver.setFacetRegistry(address(facetRegistryProxy));
         facetRegistryReceiver.setPoolRegistry(address(poolRegistryProxy));
 
@@ -61,6 +53,6 @@ contract BaseDeployment is Script {
         console.log("PoolRegistry Proxy:", address(poolRegistryProxy));
         console.log("FacetRegistryReceiver:", address(facetRegistryReceiver));
         console.log("CCIP Router Used:", BASE_CCIP_ROUTER);
-        console.log("Owner (all contracts):", deployer);
+        console.log("Owner (all contracts):", registryDeployer);
     }
 }
