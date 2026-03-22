46968561841377e6a04c9f96bdbd4becc7f2cd08
beebozy <musahabeeblai@gmail.com>
2026-03-22 13:48:51 -0700

    added unit test integration and fuzz test for the IndexFacet

--- src/interfaces/ICCIP.sol
+++ src/interfaces/ICCIP.sol
@@ -36,12 +36,18 @@ library Client {
 }
 
 interface IRouterClient {
-    function ccipSend(uint64 destinationChainSelector, Client.EVM2AnyMessage calldata message)
+    function ccipSend(
+        uint64 destinationChainSelector,
+        Client.EVM2AnyMessage calldata message
+    )
         external
         payable
         returns (bytes32);
 
-    function getFee(uint64 destinationChainSelector, Client.EVM2AnyMessage calldata message)
+    function getFee(
+        uint64 destinationChainSelector,
+        Client.EVM2AnyMessage calldata message
+    )
         external
         view
         returns (uint256 fee);
