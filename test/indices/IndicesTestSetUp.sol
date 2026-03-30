6f6bf4be5056db81740a2332698e026d103bf5bb
hoBabu1 <amansahkumar004@gmail.com>
2026-03-30 19:37:27 +0530

    test: add unit tests for MarketCapWeighted weight calculation edge cases

--- test/indices/IndicesTestSetUp.sol
+++ test/indices/IndicesTestSetUp.sol
@@ -7,7 +7,7 @@ import { MockOracle } from "../mock/MockPriceFeed.sol";
 import { IndexComponentRegistry } from "../../src/indices/IndexComponentRegistry.sol";
 import { IndexCalculationRegistry } from "../../src/indices/IndexCalculationRegistry.sol";
 import { CirculatingSupply } from "../../src/indices/CirculatingSupply.sol";
-
+import { MarketCapWeighted } from "../../src/indices/indexCalculations/MarketCapWeighted.sol";
 // ═══════════════════════════════════════════════════════════════════════
 //                         SetUp
 // ═══════════════════════════════════════════════════════════════════════
@@ -16,12 +16,13 @@ contract IndicesTestSetUp is Test {
     IndexComponentRegistry icr;
     IndexCalculationRegistry indexCalRegistry;
     CirculatingSupply cirSupply;
+    MarketCapWeighted marketCapWeighted;
 
     MockOracle btcPriceFeed;
     MockOracle ethPriceFeed;
 
     uint256 btcPrice = 70_000e18;
-    uint256 ethPrice = 3000e18;
+    uint256 ethPrice = 3_000e18;
 
     address owner = makeAddr("owner");
     address btcAddress = makeAddr("BTC");
@@ -30,6 +31,7 @@ contract IndicesTestSetUp is Test {
 
     IndexComponentRegistry.Component[] components;
     string[] symbol;
+    uint256[] supply;
 
     function setUp() public virtual {
         // deploy BTC price feed
@@ -38,13 +40,13 @@ contract IndicesTestSetUp is Test {
 
         // store symbol in array
         symbol.push("BTC");
+        supply.push(1);
         symbol.push("ETH");
+        supply.push(2);
 
         // deploy componentRegistry
         icr = new IndexComponentRegistry(owner);
-        indexCalRegistry = new IndexCalculationRegistry(owner);
-        cirSupply = new CirculatingSupply(owner,updater);
-        assertEq(updater, cirSupply.updater());
+
         // component
         components.push(
             IndexComponentRegistry.Component({
@@ -57,5 +59,27 @@ contract IndicesTestSetUp is Test {
                 symbol: "ETH", tokenAddress: ethAddress, priceFeedAddress: address(ethPriceFeed), heartbeat: 3600
             })
         );
+        // register component
+        vm.startPrank(owner);
+        icr.registerComponents(components);
+        vm.stopPrank();
+
+        // deploy Indec Calculation Registry
+        indexCalRegistry = new IndexCalculationRegistry(owner);
+
+        // deploy CirculatingSupply
+        cirSupply = new CirculatingSupply(owner, updater);
+        assertEq(updater, cirSupply.updater());
+
+        // add supply
+        vm.startPrank(updater);
+        cirSupply.updateBatch(symbol, supply);
+        vm.stopPrank();
+
+        // deploy MarketCapWeighted
+
+        marketCapWeighted = new MarketCapWeighted(address(icr), address(cirSupply));
+        assertEq(marketCapWeighted.getCirculatingSupply(), address(cirSupply));
+        assertEq(marketCapWeighted.getIndexComponentRegistry(), address(icr));
     }
 }
